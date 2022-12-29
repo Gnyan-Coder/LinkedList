8c019bc285c4741649ba594de593c12c694a279b
Gnyan-Coder <gnyanranjanbehera@gmail.com>
2022-12-29 11:18:12 +0530

    apna college in linked list all operation

---
+++ LinkedCollection.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class LinkedCollection {
+    public static void main(String[] args) {
+        LinkedList<Integer> list=new LinkedList<Integer>();
+        list.addFirst(1);
+        list.addFirst(2);
+        System.out.println(list);
+        list.addLast(10);
+        list.addLast(20);
+        System.out.println(list);
+        
+    }
+}
